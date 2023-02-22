77502cd1dc80cd9ced20f549abc4ad77a6ac2614
Mat <mathieu@redapesolutions.com>
2023-02-22 13:45:50 +0800

    default config path

--- elm_framework_helpers/config.py
+++ elm_framework_helpers/config.py
@@ -2,7 +2,7 @@ from os import getenv
 from pathlib import Path
 
 # This will be the root where we read key and secret from
-config_path = getenv('CONFIG_PATH', '/var/run')
+config_path = getenv('CONFIG_PATH', './config_path')
 
 def get_path(path: str) -> Path:
   return Path(f'{config_path}/{path}')
